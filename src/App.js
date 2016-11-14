d76a34e11d7340a3cb2e2f2ee7c7be3f045d9c09
Nathan Phelps <nathanphelps@Nathans-MacBook-Pro.local>
2016-11-14 16:18:51 -0600

    Successfully translating and displaying markdown

--- src/App.js
+++ src/App.js
@@ -1,18 +1,28 @@
 import React, { Component } from 'react';
+import marked from 'marked';
 import logo from './logo.svg';
 import './App.css';
 
 class App extends Component { 
+  constructor () {
+    super();
+    this.state = {markedDown: ''};
+    this.doTheMarkdown = this.doTheMarkdown.bind(this);
+  }
+  doTheMarkdown (event) {
+    let markyMark = marked(event.target.value);
+    this.setState({markedDown: markyMark});
+    document.getElementById('markedDown').innerHTML = this.state.markedDown;
+  }
   render() {
     return (
       <div className="App">
         <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to AMERICA!</h2>
+          <h2>It works, okay?</h2>
         </div>
-        <p className="App-intro">
-          It's Trrr-RUMP time baby!!!
-        </p>
+        <textarea rows="" cols="" onChange={this.doTheMarkdown} onKeyUp={this.doTheMarkdown}></textarea>
+        <div id="markedDown"></div>
       </div>
     );
   }
