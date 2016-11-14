aa28226994742e0146993a6858bd538c092a47a3
Nathan Phelps <nathanphelps@Nathans-MacBook-Pro.local>
2016-11-14 16:54:44 -0600

    Create gulpfile for sass

---
+++ gulpfile.js
@@ -0,0 +1,12 @@
+let gulp = require('gulp');
+let sass = require('gulp-sass');
+
+gulp.task('sass', function () {
+    return gulp.src('src/*.sass')
+    .pipe(sass())
+    .pipe(gulp.dest('public/*.css'));
+});
+
+gulp.task('sass-watch', ['sass'], function () {
+    gulp.watch('src/*.css', ['sass']);
+});
